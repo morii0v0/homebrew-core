bc8f7a50de61f5034de3730a378a091bd05d596f
baibaiyo <150710591+morii0@users.noreply.github.com>
2026-03-13 01:54:08 +0800

    Create mesonlsp formula and optimize all formula's definition

--- Formula/apache-cxf.rb
+++ Formula/apache-cxf.rb
@@ -1,65 +1,68 @@
+# frozen_string_literal: true
+
+# Openvpn3 is a hombrew Formula class.
 class ApacheCxf < Formula
-  desc "Apache CXF - an open source services framework"
-  homepage "https://cxf.apache.org"
-  version "4.1.3"
+  desc 'Apache CXF - an open source services framework'
+  homepage 'https://cxf.apache.org'
+  version '4.1.3'
   url "https://archive.apache.org/dist/cxf/#{version}/apache-cxf-#{version}.tar.gz"
-  sha256 "70ec09e5431e4833f923efe5f2206859e10f14fb1b4b56f7f0b1358f94751270"
-  license "Apache-2.0"
+  sha256 '70ec09e5431e4833f923efe5f2206859e10f14fb1b4b56f7f0b1358f94751270'
+  license 'Apache-2.0'
   revision 1
 
-  depends_on "openjdk"
+  depends_on 'openjdk'
 
   def install
-    rm_f Dir["bin/*.bat"]
-    
-    libexec.install Dir["*"]
+    rm_f Dir['bin/*.bat']
+
+    libexec.install Dir['*']
 
     Dir["#{libexec}/bin/*"].each do |f|
-    next if File.directory?(f) || File.extname(f) == ".bat"
-    
-    content = File.read(f)
-    
-    if content.include?("me=`basename $0`")
-      content.gsub!(
-        /(cxf_home=)(\$CXF_HOME|"\$CXF_HOME"|'\$CXF_HOME')/,
-        #"\\1\\${CXF_HOME:-#{libexec}}"
-        "\\1${CXF_HOME:-#{opt_libexec}}"
-      )
-      
-      unless content.include?("cxf_home=")
+      next if File.directory?(f) || File.extname(f) == '.bat'
+
+      content = File.read(f)
+
+      if content.include?('me=`basename $0`')
         content.gsub!(
-          /(me=`basename \$0`)/,
-          #"\\1\ncxf_home=\\${CXF_HOME:-#{libexec}}"
-          "\\1\ncxf_home=${CXF_HOME:-#{opt_libexec}}"
+          /(cxf_home=)(\$CXF_HOME|"\$CXF_HOME"|'\$CXF_HOME')/,
+          # "\\1\\${CXF_HOME:-#{libexec}}"
+          "\\1${CXF_HOME:-#{opt_libexec}}"
         )
+
+        unless content.include?('cxf_home=')
+          content.gsub!(
+            /(me=`basename \$0`)/,
+            # "\\1\ncxf_home=\\${CXF_HOME:-#{libexec}}"
+            "\\1\ncxf_home=${CXF_HOME:-#{opt_libexec}}"
+          )
+        end
+      end
+
+      # File.open(f, "w") { |file| file.write(content) }
+      File.write(f, content)
+
+      # File.chmod(0755, f)
+
+      # if File.read(f).include?("cxf_home=${CXF_HOME:-#{libexec}}")
+      if File.read(f).include?("cxf_home=${CXF_HOME:-#{opt_libexec}}")
+        ohai "Successfully modified #{f}"
+      else
+        opoo "Failed to modify #{f}"
       end
     end
-    
-    #File.open(f, "w") { |file| file.write(content) }
-    File.write(f, content)
-    
-    #File.chmod(0755, f)
 
-    #if File.read(f).include?("cxf_home=${CXF_HOME:-#{libexec}}")
-    if File.read(f).include?("cxf_home=${CXF_HOME:-#{opt_libexec}}")
-      ohai "Successfully modified #{f}"
-    else
-      opoo "Failed to modify #{f}"
-    end
-  end
-    
     bin.install_symlink Dir["#{libexec}/bin/*"]
-    
-    (bin/"cxf-env").write <<~EOS
+
+    (bin / 'cxf-env').write <<~EOS
       #!/bin/bash
       export CXF_HOME="#{libexec}"
       #export PATH="#{libexec}/bin:$PATH"
       exec "$@"
     EOS
-    chmod 0755, bin/"cxf-env"
+    chmod 0o755, bin / 'cxf-env'
 
-    (prefix/"samples").make_relative_symlink(libexec/"samples")
-    #prefix.install_symlink libexec/"examples" => "samples"
+    (prefix / 'samples').make_relative_symlink(libexec / 'samples')
+    # prefix.install_symlink libexec/"examples" => "samples"
   end
 
   def caveats
@@ -80,8 +83,8 @@ class ApacheCxf < Formula
   end
 
   test do
-    system "#{Formula["openjdk"].opt_bin}/java", "-version"
-    
-    system "#{bin}/cxf-env", "java", "-cp", "#{libexec}/lib/cxf-core-#{version}.jar", "-version"
+    system "#{Formula['openjdk'].opt_bin}/java", '-version'
+
+    system "#{bin}/cxf-env", 'java', '-cp', "#{libexec}/lib/cxf-core-#{version}.jar", '-version'
   end
 end
---
+++ Formula/mesonlsp.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# MesonLsp is a hombrew Formula class which installs the Meson language server.
+class Mesonlsp < Formula
+  desc 'Meson LSP - An unofficial, unendorsed language server for meson written in C++'
+  homepage 'https://github.com/JCWasmx86/mesonlsp.git'
+  version '4.3.7'
+  license 'GPL-3.0'
+
+  on_arm do
+    url "https://github.com/JCWasmx86/mesonlsp/releases/download/v#{version}/mesonlsp-aarch64-apple-darwin.zip"
+    sha256 '094ffaa4aebecd17651334b8218a38b965fa262de1ccc5cebffa88ffcc3590aa'
+  end
+
+  on_intel do
+    url "https://github.com/JCWasmx86/mesonlsp/releases/download/v#{version}/mesonlsp-x86_64-apple-darwin.zip"
+    sha256 '61f3876025f4dbca1170fcc97dbd42ab65a65bb8afc297c66b9cce4cc0bed0d3'
+  end
+
+  def install
+    libexec.install Dir['*']
+
+    bin.install_symlink Dir["#{libexec}/mesonlsp"]
+  end
+
+  test do
+    system "#{Formula['mesonlsp'].opt_bin}/mesonlsp", '--help'
+  end
+end
