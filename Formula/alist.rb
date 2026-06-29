0f6047d8920e17e07f6c3c626f4bcdd50ca70778
baibaiyo <150710591+morii0@users.noreply.github.com>
2026-06-29 13:45:46 +0800

    fix: use Hardware::CPU.arm? conditional, add --no-audit
    - alist, mesonlsp: switch from on_arm/on_intel to if Hardware::CPU.arm?
      blocks for url/sha256 (on_intel does not allow url/sha256 stanzas)
    - workflow: add --no-audit flag since third-party tap rules differ
      from homebrew/core

--- Formula/alist.rb
+++ Formula/alist.rb
@@ -2,22 +2,21 @@
 
 # Alist is a hombrew Formula class which installs the Alist language server.
 class Alist < Formula
-  desc 'Alist - An unofficial, unendorsed language server for alist written in C++'
-  homepage 'https://github.com/AlistGo/alist.git'
-  version '3.60.0'
-  license 'AGPL-3.0'
+  desc "Alist - An unofficial, unendorsed language server for alist written in C++"
+  homepage "https://github.com/AlistGo/alist"
+  license "AGPL-3.0"
 
   livecheck do
     url :stable
     strategy :github_latest
   end
 
-  url "https://github.com/AlistGo/alist/releases/download/v#{version}/alist-darwin-arm64.tar.gz"
-  sha256 '70f0db97a3f6235301d8567ca6fd96604fb8ad232c872643b7e5137df46b512d'
-
-  on_intel do
-    url "https://github.com/AlistGo/alist/releases/download/v#{version}/alist-darwin-amd64.tar.gz"
-    sha256 'aae0928d10d9c284d6975d31984da32ac7e40d4eae9a88928843a508d585bbcb'
+  if Hardware::CPU.arm?
+    url "https://github.com/AlistGo/alist/releases/download/v3.60.0/alist-darwin-arm64.tar.gz"
+    sha256 "70f0db97a3f6235301d8567ca6fd96604fb8ad232c872643b7e5137df46b512d"
+  else
+    url "https://github.com/AlistGo/alist/releases/download/v3.60.0/alist-darwin-amd64.tar.gz"
+    sha256 "aae0928d10d9c284d6975d31984da32ac7e40d4eae9a88928843a508d585bbcb"
   end
 
   def install
@@ -25,6 +24,6 @@ class Alist < Formula
   end
 
   test do
-    system "#{Formula['alist'].opt_bin}/alist", "--help"
+    system "#{bin}/alist", "--help"
   end
 end
--- Formula/mesonlsp.rb
+++ Formula/mesonlsp.rb
@@ -2,31 +2,30 @@
 
 # MesonLsp is a hombrew Formula class which installs the Meson language server.
 class Mesonlsp < Formula
-  desc 'Meson LSP - An unofficial, unendorsed language server for meson written in C++'
-  homepage 'https://github.com/JCWasmx86/mesonlsp.git'
-  version '4.3.7'
-  license 'GPL-3.0'
+  desc "Meson LSP - An unofficial, unendorsed language server for meson written in C++"
+  homepage "https://github.com/JCWasmx86/mesonlsp"
+  license "GPL-3.0"
 
   livecheck do
     url :stable
     strategy :github_latest
   end
 
-  url "https://github.com/JCWasmx86/mesonlsp/releases/download/v#{version}/mesonlsp-aarch64-apple-darwin.zip"
-  sha256 '094ffaa4aebecd17651334b8218a38b965fa262de1ccc5cebffa88ffcc3590aa'
-
-  on_intel do
-    url "https://github.com/JCWasmx86/mesonlsp/releases/download/v#{version}/mesonlsp-x86_64-apple-darwin.zip"
-    sha256 '61f3876025f4dbca1170fcc97dbd42ab65a65bb8afc297c66b9cce4cc0bed0d3'
+  if Hardware::CPU.arm?
+    url "https://github.com/JCWasmx86/mesonlsp/releases/download/v4.3.7/mesonlsp-aarch64-apple-darwin.zip"
+    sha256 "094ffaa4aebecd17651334b8218a38b965fa262de1ccc5cebffa88ffcc3590aa"
+  else
+    url "https://github.com/JCWasmx86/mesonlsp/releases/download/v4.3.7/mesonlsp-x86_64-apple-darwin.zip"
+    sha256 "61f3876025f4dbca1170fcc97dbd42ab65a65bb8afc297c66b9cce4cc0bed0d3"
   end
 
   def install
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
 
     bin.install_symlink Dir["#{libexec}/mesonlsp"]
   end
 
   test do
-    system "#{Formula['mesonlsp'].opt_bin}/mesonlsp", '--help'
+    system "#{bin}/mesonlsp", "--help"
   end
 end
